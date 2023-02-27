09c5ef97b054b27a252c797a31ba37b5623aa3e2
danielebd <danielebalebalestro@gmail.com>
2023-02-28 00:43:54 +0100

    creazione sidebar

--- js/main.js
+++ js/main.js
@@ -1,37 +1,56 @@
 'use strict';
 
 const imgs = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp',];
+const slider = document.querySelector('.slider');
+const sideBar = document.querySelector('.sidebar');
 
 let itemContent = '';
-let active = 0;
-
-const slider = document.querySelector('.slider');
+let itemContentMini = '';
 
 for(let i = 0; i < imgs.length; i++){
-    console.log(imgs[i]);
     itemContent += `<div class="item"><img src="img/${imgs[i]}" alt=""></div>`;
+    itemContentMini += `<div class="item-side"><img src="img/${imgs[i]}" alt=""></div>`;
+    //console.log(itemContentMini)
 }
 
-
+sideBar.innerHTML = sideBar.innerHTML + itemContentMini;
 slider.innerHTML = slider.innerHTML + itemContent;
-document.querySelector('.item').classList.add('show');
+//console.log(sideBar.innerHTML)
+
 
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
+let active = 0;
+
 const items = document.querySelectorAll('.item');
-console.log(items);
-console.log(items.length);
+items[active].classList.add('show');
+
+const itemsMini = document.querySelectorAll('.item-side');
+itemsMini[active].classList.add('solid');
+
+
 
 next.addEventListener('click', function() {
     if(active < items.length -1){
         items[active].classList.remove('show');
+        itemsMini[active].classList.remove('solid');
+
         active++;
+
         items[active].classList.add('show');
+        itemsMini[active].classList.add('solid');
+
+
+
     }
     else if(active === items.length -1){
         items[active].classList.remove('show');
+        itemsMini[active].classList.remove('solid');
+
         active = 0;
+
         items[active].classList.add('show');  
+        itemsMini[active].classList.add('solid');  
     }
 
 })
@@ -39,13 +58,22 @@ next.addEventListener('click', function() {
 prev.addEventListener('click', function() {
     if(active > 0){
         items[active].classList.remove('show');
+        itemsMini[active].classList.remove('solid');
+
         active--;
+
         items[active].classList.add('show');
+        itemsMini[active].classList.add('solid');
+
     }
     else if(active === 0){
         items[active].classList.remove('show');
+        itemsMini[active].classList.remove('solid');
+
         active = items.length - 1;
+
         items[active].classList.add('show');  
+        itemsMini[active].classList.add('solid');  
     }
 
 })
