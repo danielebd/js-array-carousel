ecc3b21d8e030ffb24556bed372107729d6d34fc
danielebd <danielebalebalestro@gmail.com>
2023-02-27 18:57:17 +0100

    js milestone3

--- js/main.js
+++ js/main.js
@@ -1,25 +1,40 @@
 'use strict';
 
-/* 
-<div class="item show"><img src="img/01.webp" alt=""></div>
-<div class="item"><img src="img/02.webp" alt=""></div>
-<div class="item"><img src="img/03.webp" alt=""></div>
-<div class="item"><img src="img/04.webp" alt=""></div>
-<div class="item"><img src="img/05.webp" alt=""></div> 
-*/
-
-const img = ['01webp', '02webp', '03webp', '04webp', '05webp',];
+const imgs = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp',];
 
 let itemContent = '';
+let active = 0;
 
 const slider = document.querySelector('.slider');
 
-for(let i = 0; i < img.length; i++){
-    console.log(img[i]);
-    itemContent += '<div class="item"><img src="img/01.webp" alt=""></div>';
-    
+for(let i = 0; i < imgs.length; i++){
+    console.log(imgs[i]);
+    itemContent += `<div class="item"><img src="img/${imgs[i]}" alt=""></div>`;
 }
 
 
 slider.innerHTML = slider.innerHTML + itemContent;
 document.querySelector('.item').classList.add('show');
+
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const items = document.querySelectorAll('.item');
+console.log(items)
+
+next.addEventListener('click', function() {
+    if(active < items.length -1){
+        items[active].classList.remove('show');
+        active++;
+        items[active].classList.add('show');
+    }
+
+})
+
+prev.addEventListener('click', function() {
+    if(active > 0){
+        items[active].classList.remove('show');
+        active--;
+        items[active].classList.add('show');
+    }
+
+})
